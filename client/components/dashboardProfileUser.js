646df51e98c20984de646e03a071332ec7d3c378
Vladislav Latanov <lvn1722@yandex.ru>
2022-05-28 14:46:02 +0300

    modified Profile component

--- client/components/dashboardProfileUser.js
+++ client/components/dashboardProfileUser.js
@@ -10,9 +10,11 @@ const Profile = () => {
       <div className="flex items-center justify-center h-screen">
         <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
           <div id="title">
-              Profile
-            <Link to="/dashboard"> Go To Root</Link>
-            <Link to="/dashboard/main"> Go To Main</Link>
+            Profile
+          </div>
+          <div>
+          <Link to="/dashboard"> Go To Root</Link>
+          <Link to="/dashboard/main"> Go To Main</Link>
           </div>
           <div id="username">
             { username }
